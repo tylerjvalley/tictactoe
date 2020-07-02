80f00606b5b931469573191d27c551fa205ba2df
Tyler Valley <tvalley93@gmail.com>
2020-07-02 14:18:55 -0500

    spin wheel color

--- src/app/components/spin-wheel/spin-wheel.component.ts
+++ src/app/components/spin-wheel/spin-wheel.component.ts
@@ -9,16 +9,29 @@ import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 })
 export class SpinWheelComponent implements OnInit {
   faCaretDown = faCaretDown;
+  color:string;
 
   constructor( readonly router: Router ) {}
 
   ngOnInit() {}
 
   spin() {
-    const x:number = 1024; //min value
-    const y:number = 9999; //max value
+    const x:number = 5004; //min value
+    const y:number = 5364; //max value
 
     const deg:number = Math.floor(Math.random() * (x - y)) + y;
+
+    if ( deg <= 5075 ) {
+      this.color = 'red';
+    } else if ( deg >= 5076 && deg <= 5148 ) {
+      this.color = 'yellow';
+    } else if ( deg >= 5149 && deg <= 5220 ) {
+      this.color = 'blue';
+    } else if ( deg >= 5221 && deg <= 5292 ) {
+      this.color = 'white';
+    } else if ( deg >= 5293 ) {
+      this.color = 'green';
+    }
 
     document.getElementById("wheel").style.transform = "rotate(" + deg + "deg)";
   };
