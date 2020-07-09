0b97ebc94cd937b92f9ae5cad16bb36e8c8fa5f1
Tyler Valley <tvalley93@gmail.com>
2020-07-09 15:27:06 -0500

    change spinning speed

--- src/app/components/spin-wheel/spin-wheel.component.ts
+++ src/app/components/spin-wheel/spin-wheel.component.ts
@@ -18,37 +18,17 @@ export class SpinWheelComponent implements OnInit {
   ngOnInit() {}
 
   spin() {
-      const wheel = document.getElementById("wheel");
-      const prizes = document.getElementById("prizes");
       const box = document.getElementById("main-box");
-      const x: number = 2004; //min value
-      const y: number = 2364; //max value
+      const x: number = 1004; //min value
+      const y: number = 1364; //max value
       this.spins += 1;
 
       this.deg = Math.floor(Math.random() * (x - y)) + y;
 
       if (this.spins === 1) {
         box.style.transform = "rotate(" + this.deg + "deg)";
-      } else if (this.spins === 2) {
-        box.style.transform = "rotate(" + (this.deg + 2400) + "deg)";
-      } else if (this.spins === 3) {
-        box.style.transform = "rotate(" + (this.deg + 3800) + "deg)";
-      } else if (this.spins === 4) {
-        box.style.transform = "rotate(" + (this.deg + 4600) + "deg)";
-      } else if (this.spins === 5) {
-        box.style.transform = "rotate(" + (this.deg + 5200) + "deg)";
-      } else if (this.spins === 6) {
-        box.style.transform = "rotate(" + (this.deg + 9400) + "deg)";
-      } else if (this.spins === 7) {
-        box.style.transform = "rotate(" + (this.deg + 17800) + "deg)";
-      } else if (this.spins === 8) {
-        box.style.transform = "rotate(" + (this.deg + 44600) + "deg)";
-      } else if (this.spins === 9) {
-        box.style.transform = "rotate(" + (this.deg + 791200) + "deg)";
-      } else if (this.spins === 2) {
-        box.style.transform = "rotate(" + (this.deg + 1382400) + "deg)";
+      } else if (this.spins >= 2) {
+        box.style.transform = "rotate(" + (this.deg * (this.spins)) + "deg)";
       }
-
-
    }
 }
