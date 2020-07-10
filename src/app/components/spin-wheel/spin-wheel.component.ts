2fb846b3ae8803a2e32186f64932cd9a94488ab9
Tyler Valley <tvalley93@gmail.com>
2020-07-10 16:04:09 -0500

    change formula for rotation

--- src/app/components/spin-wheel/spin-wheel.component.ts
+++ src/app/components/spin-wheel/spin-wheel.component.ts
@@ -21,37 +21,37 @@ export class SpinWheelComponent implements OnInit {
   spin() {
       const box = document.getElementById("main-box");
       const baseRotations = 5;
-      const x: number = 1; //min value
-      const y: number = 1.360; //max value
-      const randomNumber = Math.random() * (x - y) + y
+      const randomNumber = Math.random();
       const totalDegrees = (baseRotations + randomNumber) * 360;
       let prize:string;
       this.spins += 1;
 
-      console.log(randomNumber);
+      console.log(totalDegrees)
+      const rotation = totalDegrees;
+
       box.style.transform = "rotate(" + (totalDegrees * this.spins) + "deg)";
 
 
-      if (randomNumber >= 1.00 && randomNumber <= 1.075) {
+      if (rotation >= 1800 && rotation <= 1836) { //.036
         prize = "Silver Coins";
-      } else if (randomNumber >= 1.076 && randomNumber <= 1.086) {
+      } else if (rotation >= 1837 && rotation <= 1873) { //.035
         prize = "Gold Coins";
-      } else if (randomNumber >= 1.087 && randomNumber <= 1.097) {
-        prize = "A Chest";
-      } else if (randomNumber >= 1.098 && randomNumber <= 1.198) {
+      } else if (rotation >= 1874 && rotation <= 1910) { //.034
+        prize = "Chest";
+      } else if (rotation >= 1911 && rotation <= 1947) { //.033
         prize = "Re-Spin";
-      } else if (randomNumber >= 1.199 && randomNumber <= 1.209) {
-        prize = "A Chest";
-      } else if (randomNumber >= 1.210 && randomNumber <= 1.220) {
-        prize = "Silver Coins";
-      } else if (randomNumber >= 1.221 && randomNumber <= 1.231) {
+      } else if (rotation >= 1948 && rotation <= 1984) { //.032
+        prize = "Chest";
+      } else if (rotation >= 1985 && rotation <= 2021) { //.031
+         prize = "Silver Coins";
+      } else if (rotation >= 2022 && rotation <= 2058) { //.030
         prize = "Gold Coins";
-      } else if (randomNumber >= 1.232 && randomNumber <= 1.242) {
-        prize = "A Bronze Coin";
-      } else if (randomNumber >= 1.243 && randomNumber <= 1.253) {
-        prize = "A big chest";
-      } else if (randomNumber >= 1.254 && randomNumber <= 1.360) {
-        prize = "A Bronze Coin";
+      } else if (rotation >= 2059 && rotation <= 2095) { //.029
+        prize = "Bronze Coin";
+      } else if (randomNumber >= 2096 && rotation <= 2132) { //.028
+        prize = "Big Chest";
+      } else if (rotation >= 2133 && rotation <= 2160) { //.027
+        prize = "Bronze Coin";
       }
 
       setTimeout(function() {
