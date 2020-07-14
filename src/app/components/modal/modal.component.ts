1c9ea4123fdc4f5442eac034b90aca2fba1a2c76
Tyler Valley <tvalley93@gmail.com>
2020-07-14 15:09:01 -0500

    add chest and change animation

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -13,9 +13,22 @@ export class ModalComponent implements OnInit {
   }
 
   modal:boolean = false;
+  chestClosed:boolean = true;
+  chestOpen:boolean = false;
 
-  toggleModal() {
+  openModal() {
     this.modal = !this.modal;
+
+     setTimeout(() => {
+       this.chestClosed = false;
+       this.chestOpen = true;
+     }, 2000);
+  }
+
+  closeModal() {
+    this.modal = !this.modal;
+    this.chestClosed = !this.chestClosed;
+    this.chestOpen = !this.chestOpen;
   }
 
 }
